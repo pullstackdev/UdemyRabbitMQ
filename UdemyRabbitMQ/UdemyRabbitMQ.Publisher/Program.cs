bb6f9183cc23cdb0d52b3c43a49e5529facdf0de
fk <webnetdigi@outlook.com>
2023-01-09 12:17:50 +0300

    Add existing project files to Git

---
+++ UdemyRabbitMQ/UdemyRabbitMQ.Publisher/Program.cs
@@ -0,0 +1,42 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace UdemyRabbitMQ.Publisher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //rabbitmq'ye bağlanma ayarı
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://qaqduwwf:w15QbvWWXVANGQQgNrLkOD3KhZ7BTxp4@woodpecker.rmq.cloudamqp.com/qaqduwwf");//amqp url'i
+            //bağlantı sağlamak için
+            using var connection = factory.CreateConnection();
+            //rabbitmq'ya kanal üzerinden bağlanılır, kanal oluştur
+            var channel = connection.CreateModel();
+            //kanal üzerinden kuyruk oluşacak
+            channel.QueueDeclare("hello-queue", true, false, false);//true:ram değil fiziksel tut restart olsa bile kalsın. false:bu kuyruğa başka kanallardanda ulaşılabilsin.
+
+            #region WORK QUEUE
+            Enumerable.Range(1, 50).ToList().ForEach(x =>
+            {
+                string message = $"Message {x}";
+                var messageBody = Encoding.UTF8.GetBytes(message);
+                channel.BasicPublish(string.Empty, "hello-queue", null, messageBody);
+                Console.WriteLine($"Message Gönderildi: {message}");
+            });
+            #endregion
+
+            //mesajı ayarla
+            string message = "hello world";
+            //rabbitmq byte türünden herşeyi mesaj olarak alabilir, ayarlaması yapılacak
+            var messageBody = Encoding.UTF8.GetBytes(message);
+            //kanal üzerinden kuyruğa mesaj pushlanacak
+            channel.BasicPublish(string.Empty, "hello-queue", null, messageBody);
+            Console.WriteLine("Message Gönderildi");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ UdemyRabbitMQ/UdemyRabbitMQ.Subscriber/Program.cs
@@ -0,0 +1,46 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace UdemyRabbitMQ.Subscriber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //rabbitmq'ye bağlanma ayarı
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://qaqduwwf:w15QbvWWXVANGQQgNrLkOD3KhZ7BTxp4@woodpecker.rmq.cloudamqp.com/qaqduwwf");//amqp url'i
+            //bağlantı sağlamak için
+            using var connection = factory.CreateConnection();
+            //rabbitmq'ya kanal üzerinden bağlanılır, kanal oluştur
+            var channel = connection.CreateModel();
+            //kanal üzerinden kuyruk oluşacak
+            //kalabilirde ama publisherda var gerekte kalmadı
+            //channel.QueueDeclare("hello-queue", true, false, false);//true:ram değil fiziksel tut restart
+
+            channel.BasicQos(0, 5, false); //tek seferde 1 subs'a 5 mesaj iletir, true olursa tüm subs'lara totalde 5 gönderir tek seferde, bölüştürür
+
+            //okuma yapılacak
+            var consumer = new EventingBasicConsumer(channel); //kanalı dinle
+            //bu kanal üzerinden hangi kuyruğu dinleyecek
+            channel.BasicConsume("hello-queue", false, consumer); //true:mesaj geldiğinden doğruda yanlışta işlense kuyruktan sil durumu. false olursa silme ben işlenince haber veririm
+
+            consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+            {
+                var message = Encoding.UTF8.GetString(e.Body.ToArray());//byte olarak gelen mesajı aldı çevirdi
+
+                Thread.Sleep(1500);
+
+                Console.WriteLine("Gelen Message: " + message);
+
+                //mesajın işlendiğini rq'ya iletir haber verir ki gerekirse silebilsin
+                channel.BasicAck(e.DeliveryTag, false);
+            };
+
+            Console.ReadLine();
+        }
+    }
+}
